6b418e1284cdd39c7ad868b193db6c66a628ec7b
Satyam Mall <44332322+satyamhub@users.noreply.github.com>
2026-03-20 23:24:58 +0530

    0 ms (100%) 8.9 MB (53.49%) 03-20-2026 3794-reverse-string-prefix Easy .cpp Two Pointers

---
+++ C++/Easy/3794-reverse-string-prefix/3794-reverse-string-prefix.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+class Solution {
+public:
+    string reversePrefix(string s, int k) {
+        int n=s.size();
+        int left=0;
+        int right=k-1;
+        while(left<right){
+            swap(s[left],s[right]);
+            left++;
+            right--;
+        }
+        return s;
+    }
+};

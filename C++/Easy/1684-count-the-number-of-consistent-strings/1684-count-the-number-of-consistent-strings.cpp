ed2a51e97c783a1998cf6b5727ada21e4efeda24
Satyam Mall <44332322+satyamhub@users.noreply.github.com>
2026-03-22 01:56:27 +0530

    N/A (0%) N/A (0%) 03-22-2026 1684-count-the-number-of-consistent-strings Easy .cpp Array

--- C++/Easy/1684-count-the-number-of-consistent-strings/1684-count-the-number-of-consistent-strings.cpp
+++ C++/Easy/1684-count-the-number-of-consistent-strings/1684-count-the-number-of-consistent-strings.cpp
@@ -9,12 +9,12 @@ public:
        int cnt=0; //counting which is not allowed
        for(string word: words){
           for(char ch: word){
-            if((mask>>(ch-'a')&1)==0){
+            if((mask>>(ch-'a')&1)==0){   //sliding mask value to extreme left ans taking AND with 1.
                 cnt++;
                 break;
             }
           }
        }
-       return words.size()-cnt;
+       return words.size()-cnt;   // returning total - not allowed
     }
 };

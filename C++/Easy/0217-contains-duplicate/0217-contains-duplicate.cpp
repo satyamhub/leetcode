54cda715fbbf63e152d3c29012f89bd079042288
Satyam Mall <44332322+satyamhub@users.noreply.github.com>
2026-03-22 02:25:02 +0530

    N/A (0%) N/A (0%) 03-22-2026 0217-contains-duplicate Easy .cpp Array

--- C++/Easy/0217-contains-duplicate/0217-contains-duplicate.cpp
+++ C++/Easy/0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -1,15 +1,20 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        int n=nums.size();
-        unordered_map<int,int>mpp;
-        for(int num: nums){
-            mpp[num]++;
-            if(mpp[num]>=2){
-                return true;
-            }
-        }
-        return false;
+        unordered_set<int> numSet(nums.begin(), nums.end());
+        return numSet.size() < nums.size(); 
+
+        
+        // int n=nums.size();
+        // unordered_map<int,int>mpp; //Space : O(N)
+        // for(int num: nums){    //time O(N)
+        //     mpp[num]++;     
+        //     if(mpp[num]>=2){
+        //         return true;
+        //     }
+        // }
+        // return false;
+         
         
     }
 };

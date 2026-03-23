0f4c25002c8128dbe769befabd9938c28c064a34
Satyam Mall <44332322+satyamhub@users.noreply.github.com>
2026-03-23 19:42:19 +0530

    N/A (0%) N/A (0%) 03-23-2026 0003-longest-substring-without-repeating-characters Medium .cpp Hash Table

--- C++/Medium/0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ C++/Medium/0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -6,21 +6,18 @@ public:
         int cnt=0;
         int right=0;
         int left=0;
-        unordered_map<int,int> mpp(0);
+        unordered_map<int,int> mpp;
 
         while(right<n){
             char ch=s[right];
             if(mpp[ch]!=0){
                 if(mpp[ch]>left){
-                    
                     left=mpp[ch];
                     cnt=right-left+1;
-                    cout<<cnt<<"repeat"<<endl;
                 }
             }
             
             maxi=max(maxi,right-left+1);
-            //cout<<maxi<<endl;
             mpp[ch]=right+1;
             cout<<mpp[ch]<<endl;
             right++;

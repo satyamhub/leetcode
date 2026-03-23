4fd0e68237ae06f73b50766c5f49b4eab638d513
Satyam Mall <44332322+satyamhub@users.noreply.github.com>
2026-03-23 19:41:03 +0530

    303 ms (5.99%) 11.9 MB (55.26%) 03-23-2026 0003-longest-substring-without-repeating-characters Medium .cpp Hash Table

--- C++/Medium/0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ C++/Medium/0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -3,21 +3,27 @@ public:
     int lengthOfLongestSubstring(string s) {
         int n=s.size();
         int maxi=0;
-        
-        
-        for(int i=0; i<n; i++){
-            unordered_map<int,int> mpp;
-            int cnt=0;
-            for(int j=i; j<n; j++){
-                char ch=s[j];
-                if(mpp[ch]>0){
-                    break;
+        int cnt=0;
+        int right=0;
+        int left=0;
+        unordered_map<int,int> mpp(0);
+
+        while(right<n){
+            char ch=s[right];
+            if(mpp[ch]!=0){
+                if(mpp[ch]>left){
+                    
+                    left=mpp[ch];
+                    cnt=right-left+1;
+                    cout<<cnt<<"repeat"<<endl;
                 }
-                mpp[ch]++;
-                cnt++;
             }
-            cout<<cnt<<endl;
-            maxi=max(maxi,cnt);
+            
+            maxi=max(maxi,right-left+1);
+            //cout<<maxi<<endl;
+            mpp[ch]=right+1;
+            cout<<mpp[ch]<<endl;
+            right++;
         }
        return maxi;
         

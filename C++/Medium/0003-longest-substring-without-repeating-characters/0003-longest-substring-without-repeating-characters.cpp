c4bb82305fdc82e329cc78bffd0416efd81c001e
Satyam Mall <44332322+satyamhub@users.noreply.github.com>
2026-03-23 19:51:50 +0530

    9 ms (53.24%) 11.9 MB (55.26%) 03-23-2026 0003-longest-substring-without-repeating-characters Medium .cpp Hash Table

--- C++/Medium/0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ C++/Medium/0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -1,28 +1,23 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        int n=s.size();
-        int maxi=0;
-        int cnt=0;
-        int right=0;
-        int left=0;
-        unordered_map<int,int> mpp;
+        int n = s.size();
+        int maxi = 0;
+        int right = 0, left = 0;
+        unordered_map<char, int> mpp;
 
-        while(right<n){
-            char ch=s[right];
-            if(mpp[ch]!=0){
-                if(mpp[ch]>left){
-                    left=mpp[ch];
-                    cnt=right-left+1;
-                }
+        while (right < n) {
+            char ch = s[right];
+
+            if (mpp.count(ch) && mpp[ch] >= left) {
+                left = mpp[ch] + 1;
             }
-            
-            maxi=max(maxi,right-left+1);
-            mpp[ch]=right+1;
-            cout<<mpp[ch]<<endl;
+
+            mpp[ch] = right;
+            maxi = max(maxi, right - left + 1);
+
             right++;
         }
-       return maxi;
-        
+        return maxi;
     }
 };

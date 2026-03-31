3862f140dea7f768eeec8fd7283a4669a3fe3e82
Satyam Mall <44332322+satyamhub@users.noreply.github.com>
2026-03-31 16:21:51 +0530

    0 ms (100%) 21.2 MB (7.82%) 03-31-2026 0073-set-matrix-zeroes Medium .cpp Array

--- C++/Medium/0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ C++/Medium/0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -1,26 +1,29 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-        int column=matrix[0].size();
         int row=matrix.size();
-        cout<<row<<"  "<<column;
-        vector<vector<int>> temp=matrix;
+        int column=matrix[0].size();
+        
+        int tempRow[200]={0};
+        int tempColumn[200]={0};
 
         for(int i=0; i<row; i++){
             for(int j=0; j<column; j++){
                 if(matrix[i][j]==0){
-                    for(int k=0; k<row; k++){
-                        temp[k][j]=0;
-
-                    }
-                    for(int k=0; k<column; k++){
-                        temp[i][k]=0;
-
-                    }
+                   tempColumn[j]=-1;
+                   tempRow[i]=-1;
                 }  
             }
         }
-        matrix=temp;
+
+        for(int i=0; i<row; i++){
+            for(int j=0; j<column; j++){
+                if(tempRow[i]==-1 || tempColumn[j]==-1){
+                    matrix[i][j]=0;
+                }
+            }
+        }
+        
        
     }
 };

a612490b542462ea7ebf83c2abb7d60ccead0c82
Satyam Mall <44332322+satyamhub@users.noreply.github.com>
2026-03-31 17:12:06 +0530

    N/A (0%) N/A (0%) 03-31-2026 0073-set-matrix-zeroes Medium .cpp Array

--- C++/Medium/0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ C++/Medium/0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -1,29 +1,39 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-        int row=matrix.size();
-        int column=matrix[0].size();
-        
-        int tempRow[200]={0};
-        int tempColumn[200]={0};
+        int row = matrix.size();
+        int column = matrix[0].size();
 
-        for(int i=0; i<row; i++){
-            for(int j=0; j<column; j++){
-                if(matrix[i][j]==0){
-                   tempColumn[j]=-1;
-                   tempRow[i]=-1;
-                }  
-            }
-        }
+        int col0 = -1;
 
-        for(int i=0; i<row; i++){
-            for(int j=0; j<column; j++){
-                if(tempRow[i]==-1 || tempColumn[j]==-1){
-                    matrix[i][j]=0;
+        // Step 1: mark rows and columns
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < column; j++) {
+                if (matrix[i][j] == 0) {
+                    if (j != 0) {
+                        matrix[0][j] = 0;
+                    } else {
+                        col0 = 0;
+                    }
+                    matrix[i][0] = 0;
                 }
             }
         }
-        
-       
+
+        // Step 2: update matrix using markers
+        for (int i = row - 1; i >= 0; i--) {
+            for (int j = column - 1; j >= 1; j--) {
+                if (matrix[i][0] == 0 || matrix[0][j] == 0) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+
+        // Step 3: handle first column
+        if (col0 == 0) {
+            for (int i = 0; i < row; i++) {
+                matrix[i][0] = 0;
+            }
+        }
     }
 };

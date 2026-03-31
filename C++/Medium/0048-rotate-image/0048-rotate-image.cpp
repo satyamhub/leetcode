743a7d597492d9378272871900443952e4fdb3b4
Satyam Mall <44332322+satyamhub@users.noreply.github.com>
2026-03-31 18:05:27 +0530

    0 ms (100%) 10.2 MB (69.16%) 03-31-2026 0048-rotate-image Medium .cpp Array

---
+++ C++/Medium/0048-rotate-image/0048-rotate-image.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        int row=matrix.size();
+        int column=matrix[0].size();
+
+
+        for(int i=0; i<row/2; i++){
+            for(int j=0; j<column; j++){
+                swap(matrix[i][j],matrix[row-i-1][j]);
+            }
+        }
+        
+        for(int i=0; i<row; i++){
+            for(int j=i; j<column; j++){
+                swap(matrix[i][j],matrix[j][i]);
+            }
+        }
+
+        
+        
+    }
+};

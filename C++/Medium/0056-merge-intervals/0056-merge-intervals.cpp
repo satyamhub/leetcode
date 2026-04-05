882bcd7136e5df13c9bab0846aba0a11babd3a04
Satyam Mall <44332322+satyamhub@users.noreply.github.com>
2026-04-06 03:34:16 +0530

    N/A (0%) N/A (0%) 04-06-2026 0056-merge-intervals Medium .cpp Array

--- C++/Medium/0056-merge-intervals/0056-merge-intervals.cpp
+++ C++/Medium/0056-merge-intervals/0056-merge-intervals.cpp
@@ -4,26 +4,35 @@ public:
         int n=intervals.size();
         vector<vector<int>>ans;
         sort(intervals.begin(),intervals.end());
+//----------------Brute-------------------------
+        // for(int i=0; i<n; i++){
+        //     int start=intervals[i][0];
+        //     int end=intervals[i][1];
 
-        for(int i=0; i<n; i++){
-            int start=intervals[i][0];
-            int end=intervals[i][1];
+        //     if(!ans.empty() && end<=ans.back()[1]){
+        //         continue;
+        //     }
 
-            if(!ans.empty() && end<=ans.back()[1]){
-                continue;
-            }
-
-            for(int j=i+1; j<n; j++){
-                if(intervals[j][0]<=end){
-                    end=max(end,intervals[j][1]);
-                }else{
-                    break;
-                }
+        //     for(int j=i+1; j<n; j++){
+        //         if(intervals[j][0]<=end){
+        //             end=max(end,intervals[j][1]);
+        //         }else{
+        //             break;
+        //         }
                 
+        //     }
+        //     ans.push_back({start,end});
+        // }
+        // return ans;
+        
+        //---------Optimal-------------
+        for(int i=0; i<n;  i++){
+            if(ans.empty() || intervals[i][0]>ans.back()[1]){
+                ans.push_back(intervals[i]);
+            }else{
+                ans.back()[1]=max(ans.back()[1],intervals[i][1]);
             }
-            ans.push_back({start,end});
         }
         return ans;
-        
     }
 };

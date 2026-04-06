4dfe6235a580cdc4b205a920a16f79919f81bf11
Satyam Mall <44332322+satyamhub@users.noreply.github.com>
2026-04-06 21:19:22 +0530

    N/A (0%) N/A (0%) 04-06-2026 0088-merge-sorted-array Easy .cpp Array

--- C++/Easy/0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ C++/Easy/0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -1,10 +1,31 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        for(int i=0; i<n; i++){
-            nums1[m+i]=nums2[i];
+        //----------Brute-------------------
+        // for(int i=0; i<n; i++){
+        //     nums1[m+i]=nums2[i];
+        // }
+        // sort(nums1.begin(),nums1.end());
+ 
+       //------Better------------
+        int p1=0;
+        int p2=0;
+
+        while(p1<m+n && p2<n){
+            if(p1<m){
+                if(nums1[p1]<=nums2[p2]){
+                    p1++;
+                }else{
+                    swap(nums1[p1],nums2[p2]);
+                    p1++;
+                }
+            }
+            if(p1>=m && p2<n){
+                swap(nums1[p1],nums2[p2]);
+                p1++;
+                p2++;
+            }
         }
-        sort(nums1.begin(),nums1.end());
         
     }
 };

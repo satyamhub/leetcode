f50b79634f41344e37db320c3e3f57392b356180
Satyam Mall <44332322+satyamhub@users.noreply.github.com>
2026-04-07 00:53:05 +0530

    0 ms (100%) 12.2 MB (96.41%) 04-07-2026 0088-merge-sorted-array Easy .cpp Array

--- C++/Easy/0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ C++/Easy/0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -9,25 +9,20 @@ public:
         // sort(nums1.begin(),nums1.end());
 
         //------Better------------
-        //
-        if(!nums2.empty()){
-            if(nums2[0]>=nums1[0]){
-            nums1[0]=nums2[0];
-            }
-        }
-        int last1 = m + n - 1;
-        int last2 = n - 1;
+        
         int middle1 = m - 1;
-        while (middle1 >= 0 && last2 >= 0) {
-            if (nums1[middle1] >= nums2[last2] && last2 >= 0) {
+        int last2 = n - 1;
+        int last1 = m + n - 1;
+        
+        while (last2 >= 0) {
+            if (middle1 >= 0 && nums1[middle1] > nums2[last2]) {
                 nums1[last1] = nums1[middle1];
+                last1--;
                 middle1--;
-                last1--;
-            }
-            if (nums2[last2] > nums1[middle1] && last2 >= 0) {
+            } else {
                 nums1[last1] = nums2[last2];
-                last2--;
                 last1--;
+                last2--;
             }
         }
         

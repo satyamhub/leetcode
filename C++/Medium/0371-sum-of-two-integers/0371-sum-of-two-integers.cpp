923d12b1883301f475529d7adb80578582842d7c
Satyam Mall <44332322+satyamhub@users.noreply.github.com>
2026-04-07 21:10:04 +0530

    N/A (0%) N/A (0%) 04-07-2026 0371-sum-of-two-integers Medium .cpp Math

--- C++/Medium/0371-sum-of-two-integers/0371-sum-of-two-integers.cpp
+++ C++/Medium/0371-sum-of-two-integers/0371-sum-of-two-integers.cpp
@@ -1,7 +1,11 @@
 class Solution {
 public:
     int getSum(int a, int b) {
-        return a+b;
-        
+        while (b != 0) {
+            unsigned int carry = a & b; // AND gate for carry
+            a = a ^ b;                  // XOR gate for sum
+            b = carry << 1;             // Shift carry
+        }
+        return a;
     }
 };

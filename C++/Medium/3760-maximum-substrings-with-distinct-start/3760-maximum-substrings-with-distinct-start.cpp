fbf7d3af8667a7b1524f1ae562e1a8c11c5c5c7b
Satyam Mall <44332322+satyamhub@users.noreply.github.com>
2026-04-07 13:44:51 +0530

    N/A (0%) N/A (0%) 04-07-2026 3760-maximum-substrings-with-distinct-start Medium .cpp Hash Table

--- C++/Medium/3760-maximum-substrings-with-distinct-start/3760-maximum-substrings-with-distinct-start.cpp
+++ C++/Medium/3760-maximum-substrings-with-distinct-start/3760-maximum-substrings-with-distinct-start.cpp
@@ -2,14 +2,18 @@ class Solution {
 public:
     int maxDistinct(string s) {
         unordered_map<int,int>mpp;
-        int sum=0;
+        // int sum=0;
+        // for(char ch:s){
+        //     if(mpp[ch]==0){
+        //         sum++;
+        //     }
+        //     mpp[ch]++;
+        // }
+        // return sum;
+        
         for(char ch:s){
-            if(mpp[ch]==0){
-                sum++;
-            }
             mpp[ch]++;
         }
-        return sum;
-        
+        return mpp.size();
     }
 };

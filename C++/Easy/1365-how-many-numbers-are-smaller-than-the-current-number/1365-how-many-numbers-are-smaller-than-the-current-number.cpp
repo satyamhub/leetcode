f8ea77c0b3028d7c73ffc27f3fb609b3300eee61
Satyam Mall <44332322+satyamhub@users.noreply.github.com>
2026-04-13 03:27:26 +0530

    N/A (0%) N/A (0%) 04-13-2026 1365-how-many-numbers-are-smaller-than-the-current-number Easy .cpp Array

--- C++/Easy/1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.cpp
+++ C++/Easy/1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.cpp
@@ -5,6 +5,17 @@ nums[i]>nums[j] cnt++
 and i will add cnt in vector.
 */
 
+/*----Optimal
+I will create an empty array of 101 size and all elements=0.
+Then i will iterate in nums and update the count of numbers in
+nums in array.
+then i will do cummulative sum of current index in array with
+previous count sum.
+then i will
+
+
+*/
+
 class Solution {
 public:
     vector<int> smallerNumbersThanCurrent(vector<int>& nums) {
@@ -26,15 +37,22 @@ public:
         //----Optimal-----
         int arr[101] = {0};
 
-        for (int x : nums)
-            arr[x]++;
+        for (int x : nums){
+            arr[x]++;   
+            //cout<<arr[x]<<endl;
+        }
 
-        for (int i = 1; i < 101; i++)
+        for (int i = 1; i < 101; i++){
             arr[i] += arr[i - 1];
+            cout<<arr[i]<<endl;
+        }
+            
 
         vector<int> ans;
-        for (int x : nums)
+        for (int x : nums){
+           // cout<<arr[x-1]<<endl;
             ans.push_back(x == 0 ? 0 : arr[x - 1]);
+        }
 
         return ans;
     }

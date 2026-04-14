c640f1416394b2e7693f39fac4dd42fd4f138c6f
Satyam Mall <44332322+satyamhub@users.noreply.github.com>
2026-04-14 17:36:40 +0530

    N/A (0%) N/A (0%) 04-14-2026 0035-search-insert-position Easy .cpp Array

--- C++/Easy/0035-search-insert-position/0035-search-insert-position.cpp
+++ C++/Easy/0035-search-insert-position/0035-search-insert-position.cpp
@@ -1,3 +1,23 @@
+/*
+---Brute---
+Use simple linear search to find the nums[i]>=target.
+if target is not present return ans=nums.size();
+TC:O(N)
+SC:O(1)
+*/
+
+/*
+----Optimal----
+Use binary Search to find the lower bound of the target.
+take ans=nums.size();
+then if in binary search if(nums[mid]>=target) this can be our ans,
+also go to left to check.
+
+TC:O(log base 2 N)
+SC:O(1)
+*/
+
+
 class Solution {
 public:
     int searchInsert(vector<int>& nums, int target) {

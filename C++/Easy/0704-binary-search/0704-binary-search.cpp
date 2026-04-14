96806f2d0256416871b2a75a9f16837ca5a9867c
Satyam Mall <44332322+satyamhub@users.noreply.github.com>
2026-04-14 16:38:26 +0530

    N/A (0%) N/A (0%) 04-14-2026 0704-binary-search Easy .cpp Array

--- C++/Easy/0704-binary-search/0704-binary-search.cpp
+++ C++/Easy/0704-binary-search/0704-binary-search.cpp
@@ -26,8 +26,31 @@ public:
         return -1;
     }
 
+    int better(vector<int>&nums, int target){
+        int n=nums.size();
+        int low=0;
+        int high=n-1;
+
+        while(low <= high){
+            int mid=(low+high)/2;
+            if(target==nums[mid]){
+                return mid;
+            }
+            else if(target>nums[mid]){
+                low=mid+1;
+            }
+            else{
+                high=mid-1;
+            }
+
+        }
+        return -1;
+    }
+
     int search(vector<int>& nums, int target) {
-        int ans=brute(nums,target);
-        return ans;
+        //int ansBrute=brute(nums,target);
+        int ansBetter=better(nums,target);
+        
+        return ansBetter;
     }
 };

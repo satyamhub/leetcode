6c846d0fac61b46cce44acffd48dbd89efb68326
Satyam Mall <44332322+satyamhub@users.noreply.github.com>
2026-04-17 18:48:41 +0530

    N/A (0%) N/A (0%) 04-17-2026 0033-search-in-rotated-sorted-array Medium .cpp Array

--- C++/Medium/0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ C++/Medium/0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,12 +1,54 @@
+/*
+-----Brute---
+I will linear search in the array and return the index.
+TC:O(N)
+SC:O(1)
+
+
+
+----Optimal-----
+
+*/
+
+
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        int n=nums.size();
+        //-----Brute----
+        // int n=nums.size();
 
-        for(int i=0; i<n; i++){
-            if(nums[i]==target){
-                return i;
-                break;
+        // for(int i=0; i<n; i++){
+        //     if(nums[i]==target){
+        //         return i;
+        //         break;
+        //     }
+        // }
+        // return -1;
+
+        //---Optimal------
+        int n=nums.size();
+        int low=0;
+        int high=n-1;
+
+        while(low<=high){
+            int mid=low+(high-low)/2;
+
+            if(nums[mid]==target) return mid;
+
+            //Left Array is sorted
+            if(nums[low]<=nums[mid]){
+                if(target>=nums[low] && target<=nums[mid]){
+                    high=mid-1;
+                }else{
+                    low=mid+1;
+                }
+
+            }else{
+                if(target>=nums[mid] && target<=nums[high]){
+                    low=mid+1;
+                }else{
+                    high=mid-1;
+                }
             }
         }
         return -1;

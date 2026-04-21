a87e31fbf74457ef0778a2d903951fa4030bf912
Satyam Mall <44332322+satyamhub@users.noreply.github.com>
2026-04-21 18:39:02 +0530

    0 ms (100%) 12.5 MB (71.59%) 04-21-2026 0162-find-peak-element Medium .cpp Array

--- C++/Medium/0162-find-peak-element/0162-find-peak-element.cpp
+++ C++/Medium/0162-find-peak-element/0162-find-peak-element.cpp
@@ -38,8 +38,8 @@ public:
         int n=nums.size();
         
         //check for 1st and last element.
-        if(nums[0]>nums[1]) return 0;
-        if(nums[n-2]<nums[n-1]) return n-1;
+        if(n>1 && nums[0]>nums[1]) return 0;
+        if(n>1 && nums[n-2]<nums[n-1]) return n-1;
     
         int low=1;
         int high=n-2;
@@ -50,7 +50,7 @@ public:
             if(nums[mid-1]<nums[mid] && nums[mid]>nums[mid+1]) return mid;
 
             if(nums[mid-1]>nums[mid]) high=mid-1;
-            if(nums[mid]<nums[mid+1]) low=mid+1;
+            else if(nums[mid]<nums[mid+1]) low=mid+1;
         }
        return 0;
     }

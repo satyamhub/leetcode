97dc0d08b505a34190c4e9db51fc2424cf4f4827
Satyam Mall <44332322+satyamhub@users.noreply.github.com>
2026-04-22 02:38:22 +0530

    0 ms (100%) 8.4 MB (99.33%) 04-22-2026 0069-sqrtx Easy .cpp Math

--- C++/Easy/0069-sqrtx/0069-sqrtx.cpp
+++ C++/Easy/0069-sqrtx/0069-sqrtx.cpp
@@ -1,11 +1,44 @@
+/*
+---Brute---
+I will do linear search and find i*i<=x; i++;
+return i-1;
+TC:(sqrt(X))
+SC:O(1)
+*/
+/*
+---Optimal----
+I will use binary Search.
+TC:O(log x)
+SC:O(1)
+
+*/
+
 class Solution {
 public:
     int mySqrt(int x) {
-        long long i=1;
+        //--Brute---
+        
+        // long long i=1;
 
-        while(i*i<=x){
-            i++;
+        // while(i*i<=x){
+        //     i++;
+        // }
+        // return i-1;
+
+        //----Optimal----
+        int ans=0;
+        int low=1;
+        int high=x;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(mid<=x/mid){
+                ans=mid;
+                low=mid+1;
+            }
+            else {
+                high=mid-1;
+            }
         }
-        return i-1;
+        return ans;
     }
 };

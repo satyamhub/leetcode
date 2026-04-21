f38ae05cd10c9fd453e1c2cff05992a5aee6e964
Satyam Mall <44332322+satyamhub@users.noreply.github.com>
2026-04-21 18:33:36 +0530

    N/A (0%) N/A (0%) 04-21-2026 0162-find-peak-element Medium .cpp Array

--- C++/Medium/0162-find-peak-element/0162-find-peak-element.cpp
+++ C++/Medium/0162-find-peak-element/0162-find-peak-element.cpp
@@ -6,19 +6,52 @@ SC:O(1)
 
 */
 
+/*
+----Optimal----
+I will do binary search from index 1 to 2nd last.
+if peak is in right nums[mid]<nums[mid+1]--> I will low=mid+1;
+if peak is in left nums[mid-1]>nums[mid] ---> i will high=mid-1;
+
+If nums[mid-1]<nums[mid]>nums[mid+1] i will return mid;
+TC:O(log N)
+SC:O(1)
+
+
+*/
+
 
 class Solution {
 public:
     int findPeakElement(vector<int>& nums) {
         //---Brute----
+        // int n=nums.size();
+
+        // if(n>=2 && nums[0]>nums[1]) return 0;
+        // if(n>=2 && nums[n-1]>nums[n-2]) return n-1;
+
+        // for(int i=1; i<n-1; i++){
+        //     if(nums[i-1]<nums[i] && nums[i]>nums[i+1]) return i;
+        // }
+        // return 0;
+
+        //---Optimal----
         int n=nums.size();
+        
+        //check for 1st and last element.
+        if(nums[0]>nums[1]) return 0;
+        if(nums[n-2]<nums[n-1]) return n-1;
+    
+        int low=1;
+        int high=n-2;
 
-        if(n>=2 && nums[0]>nums[1]) return 0;
-        if(n>=2 && nums[n-1]>nums[n-2]) return n-1;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            
+            if(nums[mid-1]<nums[mid] && nums[mid]>nums[mid+1]) return mid;
 
-        for(int i=1; i<n-1; i++){
-            if(nums[i-1]<nums[i] && nums[i]>nums[i+1]) return i;
+            if(nums[mid-1]>nums[mid]) high=mid-1;
+            if(nums[mid]<nums[mid+1]) low=mid+1;
         }
-        return 0;
+       return 0;
     }
 };

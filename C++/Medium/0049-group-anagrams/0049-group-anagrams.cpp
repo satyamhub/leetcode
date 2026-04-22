4109ed7920cd6e0db7201b3af188ddc0213e330d
Satyam Mall <44332322+satyamhub@users.noreply.github.com>
2026-04-23 02:30:17 +0530

    11 ms (91.02%) 24.9 MB (87.24%) 04-23-2026 0049-group-anagrams Medium .cpp Array

---
+++ C++/Medium/0049-group-anagrams/0049-group-anagrams.cpp
@@ -0,0 +1,27 @@
+/*
+I will use hashmap to store the sum of all char in a single string in a index.
+
+
+*/
+
+
+#include<bits/stdc++.h>
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+       unordered_map<string, vector<string>> ans;
+
+        for (string& s : strs) {
+            string key = s;
+            sort(key.begin(), key.end());
+            ans[key].push_back(s);
+        }
+
+        vector<vector<string>> result;
+        for (auto& entry : ans) {
+            result.push_back(entry.second);
+        }
+
+        return result;  
+    }
+};

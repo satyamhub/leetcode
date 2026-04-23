d6d5a33dbb3750a8ce60d3d39d6d1635ffe1af1d
Satyam Mall <44332322+satyamhub@users.noreply.github.com>
2026-04-23 15:29:36 +0530

    N/A (0%) N/A (0%) 04-23-2026 0347-top-k-frequent-elements Medium .cpp Array

--- C++/Medium/0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ C++/Medium/0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -11,22 +11,27 @@ public:
         int n = nums.size();
         unordered_map<int, int> mpp;
         vector<int> ans;
-
+        
+        //Inserting in Map with freq.
         for (int num : nums) {
             mpp[num]++;
         }
 
         vector<vector<int>> count;
-
+        //inserting map in 2d vector=[freq][element]
         for (auto it : mpp) {
             count.push_back({it.second, it.first});
         }
-
+        
+        //sorting the count in ascending order.
         sort(count.begin(), count.end());
+
+        //checking the count vector in cout
         for (int i = 0; i < count.size(); i++) {
             cout << count[i][0] << " " << count[i][1] << endl;
         }
-
+        
+        //pushing count from end in ans vector
         for (int i = 0; i < k ;i++) {
             ans.push_back(count[count.size()-1-i][1]);
         }

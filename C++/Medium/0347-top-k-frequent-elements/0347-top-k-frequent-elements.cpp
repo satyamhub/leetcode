74a4ebf78a0341226d3fc9d0d612bf9a577cf3a4
Satyam Mall <44332322+satyamhub@users.noreply.github.com>
2026-04-23 15:37:23 +0530

    N/A (0%) N/A (0%) 04-23-2026 0347-top-k-frequent-elements Medium .cpp Array

--- C++/Medium/0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ C++/Medium/0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -1,41 +1,39 @@
 /*
-I will use hash map to store the frequency of each element.
-Then i will insert the map in a 2d vector =[count][element]
-Then I will sort the 2d vector according to their freq.
-I will insert the count in ans vector from end;
-
-
+Approach:
+- Count the frequency of each element using a hash map.
+- Store the data as {frequency, element} pairs in a vector.
+- Sort the vector in descending order of frequency.
+- Pick the top k elements and return their corresponding values.
 */
+
 
 class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
-        int n = nums.size();
         unordered_map<int, int> mpp;
-        vector<int> ans;
         
-        //Inserting in Map with freq.
+        // Count frequency
         for (int num : nums) {
             mpp[num]++;
         }
 
         vector<vector<int>> count;
-        //inserting map in 2d vector=[freq][element]
+
+        // Store as {freq, element}
         for (auto it : mpp) {
             count.push_back({it.second, it.first});
         }
-        
-        //sorting the count in ascending order.
-        sort(count.begin(), count.end());
 
-        //checking the count vector in cout
-        for (int i = 0; i < count.size(); i++) {
-            cout << count[i][0] << " " << count[i][1] << endl;
-        }
-        
-        //pushing count from end in ans vector
-        for (int i = 0; i < k ;i++) {
-            ans.push_back(count[count.size()-1-i][1]);
+        // Sort in descending order of frequency
+        sort(count.begin(), count.end(), [](auto &a, auto &b){
+            return a[0] > b[0];
+        });
+
+        vector<int> ans;
+
+        // Take top k elements
+        for (int i = 0; i < k; i++) {
+            ans.push_back(count[i][1]);
         }
 
         return ans;

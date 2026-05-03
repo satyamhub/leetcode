19c3d269c92ae53c80400613d9cf77bd60401952
Satyam Mall <44332322+satyamhub@users.noreply.github.com>
2026-05-03 21:31:40 +0530

    50 ms (47.07%) 61.7 MB (36.96%) 05-03-2026 1552-magnetic-force-between-two-balls Medium .cpp Array

---
+++ C++/Medium/1552-magnetic-force-between-two-balls/1552-magnetic-force-between-two-balls.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    bool possible(vector<int>& position, int dist, int m){
+        int cntBalls=1;
+        int last=position[0];
+        for(int i=0; i<position.size(); i++){
+            if(position[i]-last>=dist){
+                cntBalls++;
+                last=position[i];
+            }
+            if(cntBalls>=m) return true;
+        }
+        return false;
+    }
+
+    int maxDistance(vector<int>& position, int m) {
+        sort(position.begin(),position.end());
+        int n=position.size();
+
+        int low=1;
+        int high=position[n-1]-position[0];
+
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(possible(position, mid, m)){
+                low=mid+1;
+            }else{
+                high=mid-1;
+            }
+        }
+        return high;
+    }
+};

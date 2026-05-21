13dd98185e341d3baba00ee489174d63d8fb362c
Satyam Mall <44332322+satyamhub@users.noreply.github.com>
2026-05-22 01:10:17 +0530

    0 ms (100%) 48.5 MB (81.82%) 05-22-2026 0442-find-all-duplicates-in-an-array Medium .cpp Array

--- C++/Medium/0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.cpp
+++ C++/Medium/0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.cpp
@@ -1,14 +1,33 @@
 class Solution {
 public:
     vector<int> findDuplicates(vector<int>& nums) {
+        //----Brute----
+
+        // int n=nums.size();
+        // vector<int>ans;
+        // map<int,int>mpp;
+        // for(int i=0; i<n; i++){
+        //     if(mpp[nums[i]]>0){
+        //         ans.push_back(nums[i]);
+        //     }else{
+        //         mpp[nums[i]]++;
+        //     }
+        // }
+        // return ans;
+
+        //----Optimal----
         int n=nums.size();
         vector<int>ans;
-        map<int,int>mpp;
+
         for(int i=0; i<n; i++){
-            if(mpp[nums[i]]>0){
-                ans.push_back(nums[i]);
+            int num=abs(nums[i]);
+
+            int index=num-1;
+
+            if(nums[index] < 0){
+                ans.push_back(num);
             }else{
-                mpp[nums[i]]++;
+                nums[index]*=-1;
             }
         }
         return ans;

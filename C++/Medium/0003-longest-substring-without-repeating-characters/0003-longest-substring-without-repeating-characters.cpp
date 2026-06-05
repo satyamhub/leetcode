6a3ebb73c61f29f9fe8d8c2a57ac56a672e283cc
Satyam Mall <44332322+satyamhub@users.noreply.github.com>
2026-06-06 00:17:19 +0530

    4 ms (73.69%) 11.5 MB (82.24%) 06-06-2026 0003-longest-substring-without-repeating-characters Medium .cpp Hash Table

--- C++/Medium/0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ C++/Medium/0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -1,3 +1,24 @@
+/*
+Approach:
+1. I will create a hash array of 256 size with all
+   equal to -1.
+
+2. I will put a pointer left at 0 and right moving 
+   from 0 to N.
+
+3. ALso i will put the ascii value of the char to 
+   its position in string in array.
+
+4. Whenever right pointer finds the repeating 
+   char, it will move left pointer to one ahead of 
+   repeating char. Also update current to hash.
+
+5. len=right-left+1, update to maxlen.
+
+TC:O(N)
+SC:O(1)
+*/
+
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {

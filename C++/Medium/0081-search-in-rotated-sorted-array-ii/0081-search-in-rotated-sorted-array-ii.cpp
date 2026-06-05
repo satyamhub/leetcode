097398b01f09ae3cfefd283136528fca5b0357b7
Satyam Mall <44332322+satyamhub@users.noreply.github.com>
2026-06-06 01:07:45 +0530

    0 ms (100%) 18.1 MB (35.57%) 06-06-2026 0081-search-in-rotated-sorted-array-ii Medium .cpp Array

--- C++/Medium/0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
+++ C++/Medium/0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
@@ -1,51 +1,32 @@
-/*
-----Optimal---
-I will do binary search all same as search in rotated sorted array 1.
-if(low=mid=high) shrink the checks.
-low++
-high--
-continue
-
-TC:O(N/2) worst case.
-TC:O(log n) avg case.
-SC:O(1)
-
-*/
-
-
-
 class Solution {
 public:
     bool search(vector<int>& nums, int target) {
         int n=nums.size();
-        int low=0;
-        int high=n-1;
+        int left=0;
+        int right=n-1;
 
-        while(low<=high){
-            int mid=low+(high-low)/2;
-
+        while(left<=right){
+            int mid=left+(right-left)/2;
             if(nums[mid]==target) return true;
 
-            //extra edge case
-            if(nums[low]==nums[mid] && nums[mid]==nums[high]){
-                high--;
-                low++;
+            if(nums[left]==nums[mid] && nums[mid]==nums[right]){
+                right--;
+                left++;
                 continue;
             }
 
-            //Left Array is sorted
-            if(nums[low]<=nums[mid]){
-                if(target>=nums[low] && target<=nums[mid]){
-                    high=mid-1;
+            if(nums[left]<=nums[mid]){
+                if(target>=nums[left] && target<=nums[mid]){
+                    right=mid-1;
                 }else{
-                    low=mid+1;
+                    left=mid+1;
                 }
 
             }else{
-                if(target>=nums[mid] && target<=nums[high]){
-                    low=mid+1;
+                if(target>=nums[mid] && target<=nums[right]){
+                    left=mid+1;
                 }else{
-                    high=mid-1;
+                    right=mid-1;
                 }
             }
         }

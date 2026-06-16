b9bdad2f8ff9dda0f4cd707f2ceacbe9dc6d4b2b
Satyam Mall <44332322+satyamhub@users.noreply.github.com>
2026-06-16 15:08:09 +0530

    0 ms (100%) 12.1 MB (14.45%) 06-16-2026 0014-longest-common-prefix Easy .cpp Array

--- C++/Easy/0014-longest-common-prefix/0014-longest-common-prefix.cpp
+++ C++/Easy/0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -1,3 +1,18 @@
+/*
+Approach: Optimal
+1. I will first sort the strs array.
+2. So that the smallest string goes at 1st and longest string goes last.
+3. I will compare how many character of 1st and last string matches
+4. Then I will push into the ans string if they matches.
+5. From this all in between string also matches bcz of sorting.
+
+TC:O(NlogN)+O(number of common character)
+SC:O(1)
+
+
+
+*/
+
 class Solution {
 public:
     string longestCommonPrefix(vector<string>& strs) {

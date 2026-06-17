ef2c13717fe526ec7dfdd6753c955f528f4de666
Satyam Mall <44332322+satyamhub@users.noreply.github.com>
2026-06-17 08:32:10 +0530

    0 ms (100%) 22.6 MB (80.04%) 06-17-2026 0026-remove-duplicates-from-sorted-array Easy .cpp Array

--- C++/Easy/0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
+++ C++/Easy/0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -1,3 +1,20 @@
+/*
+Approach:
+1. I will Assign two pointers left and right.
+2. 'left' will point to the unique elements and 'right' will move right and find
+    which element is not equal to nums[left].
+
+3. If unique element found left++ and swap(nums[left], nums[right]);
+4. right++;
+
+
+TC:O(N)
+SC:(1)
+
+
+*/
+
+
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {

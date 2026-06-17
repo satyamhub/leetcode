8af94193178c1002056ce97c7e5bf5605673a867
Satyam Mall <44332322+satyamhub@users.noreply.github.com>
2026-06-17 11:32:36 +0530

    7 ms (19.13%) 9.7 MB (75.72%) 06-17-2026 0242-valid-anagram Easy .cpp Hash Table

--- C++/Easy/0242-valid-anagram/0242-valid-anagram.cpp
+++ C++/Easy/0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,30 +1,9 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        unordered_map<int,int>mpp(0);
+        sort(s.begin(), s.end());
+        sort(t.begin(), t.end());
 
-        if(s.size()!=t.size()){
-            return false;
-        }
-
-        for(int i=0; i<s.size(); i++){
-            mpp[s[i]]++;
-            mpp[t[i]]--;
-        }
-
-        // for(int i=0; i<t.size(); i++){
-        //     if(mpp[t[i]]==0){
-        //         return false;
-        //     }
-        //     mpp[t[i]]--;
-        // }
-        for(auto it: mpp){
-            if(it.second!=0){
-                return false;
-            }
-        }
-
-        return true;
-             
+        return s==t;
     }
 };

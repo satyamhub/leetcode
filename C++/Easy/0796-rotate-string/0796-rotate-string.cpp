6beb085f018d07f4a4a914321d46817c48d35a74
Satyam Mall <44332322+satyamhub@users.noreply.github.com>
2026-06-17 09:17:12 +0530

    0 ms (100%) 8.2 MB (59.89%) 06-17-2026 0796-rotate-string Easy .cpp String

--- C++/Easy/0796-rotate-string/0796-rotate-string.cpp
+++ C++/Easy/0796-rotate-string/0796-rotate-string.cpp
@@ -1,14 +1,23 @@
+/*
+Approach:
+1. I will add the s with s, so that it contain all the rotations in it substring.
+2. Then i will check the substring with stl.
+
+TC:O(N)
+SC:O(1)
+
+
+*/
+
 class Solution {
 public:
     bool rotateString(string s, string goal) {
-        for(int i=0; i<s.size(); i++){
-            string temp=s;
-            rotate(temp.begin(), temp.begin()+i, temp.end());
-            if(temp==goal){
-                return true;
-            }
-        }
+        if(s.size()!=goal.size()) return false;
+
         
+        if((s+s).find(goal)!=string::npos){
+            return true;
+        }
         return false;
     }
 };

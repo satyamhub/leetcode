1919a31cdfc401ecb74748bef6fc736527055662
Satyam Mall <44332322+satyamhub@users.noreply.github.com>
2026-06-18 15:53:36 +0530

    0 ms (100%) 31.3 MB (45.75%) 06-18-2026 0704-binary-search Easy .cpp Array

--- C++/Easy/0704-binary-search/0704-binary-search.cpp
+++ C++/Easy/0704-binary-search/0704-binary-search.cpp
@@ -1,27 +1,8 @@
-/*
-----Brute----
-I will iterate from start and will search for the target and return the index.
-TC:O(N)
-SC:O(1)
-*/
-
-/*
------Better(Iterative Binary SEARCH)-----
-I will use the binary search.
-I will use three pointer, low, mid and high.
-if(target==nums[mid]) return mid.
-else if(target>nums[mid]) low=mid+1;
-else :  high=mid-1;
-
-TC:O(Log N base 2)
-SC:O(1)
-*/
-
-
 class Solution {
 public:
-    int brute(vector<int>&nums, int target){
+    int search(vector<int>& nums, int target) {
         int n=nums.size();
+        //brute
 
         for(int i=0; i<n; i++){
             if(nums[i]==target){
@@ -29,41 +10,5 @@ public:
             }
         }
         return -1;
-    }
-
-    int better(vector<int>&nums, int target){
-        int n=nums.size();
-        int low=0;
-        int high=n-1;
-
-        while(low <= high){
-            int mid=(low+high)/2;
-            if(target==nums[mid]){
-                return mid;
-            }
-            else if(target>nums[mid]){
-                low=mid+1;
-            }
-            else{
-                high=mid-1;
-            }
-
-        }
-        return -1;
-    }
-
-
-    int optimal(vector<int>nums, int low, int high, int target){
-        if(low>high) return -1;
-        int mid=(low+high)/2;
-        if(nums[mid]==target) return mid;
-        else if(nums[mid]<target) return optimal(nums, mid+1, high, target);
-        return optimal(nums, low, high-1, target);
-    }
-
-    int search(vector<int>& nums, int target) {
-        //int ansBrute=brute(nums,target);
-          return better(nums,target);
-        //  int  optimal(nums, 0, nums.size()-1, target);
     }
 };

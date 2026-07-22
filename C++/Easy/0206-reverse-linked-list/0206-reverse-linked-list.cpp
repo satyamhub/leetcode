0f6bec93ae2af29a2f658966232b1d2b67d3a44e
Satyam Mall <44332322+satyamhub@users.noreply.github.com>
2026-07-22 23:04:16 +0530

    0 ms (100%) 13.7 MB (8.92%) 07-22-2026 0206-reverse-linked-list Easy .cpp Linked List

--- C++/Easy/0206-reverse-linked-list/0206-reverse-linked-list.cpp
+++ C++/Easy/0206-reverse-linked-list/0206-reverse-linked-list.cpp
@@ -11,17 +11,13 @@
 class Solution {
 public:
     ListNode* reverseList(ListNode* head) {
-        if(head==NULL){
-            return NULL;
+        if(head==NULL || head->next==NULL){
+            return head;
         }
-        ListNode* temp=head;
-        ListNode* prev=NULL;
-        while(temp!=NULL){
-            ListNode* front=temp->next;
-            temp->next=prev;
-            prev=temp;
-            temp=front;
-        }
-        return prev;
+        ListNode* newHead= reverseList(head->next);
+        ListNode* front= head->next;
+        front->next=head;
+        head->next=NULL;
+        return newHead;
     }
 };

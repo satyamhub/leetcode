53f7472c6d374ff167acaeaca1a0e7acd4661556
Satyam Mall <44332322+satyamhub@users.noreply.github.com>
2026-07-26 10:35:04 +0530

    12 ms (8.3%) 13.8 MB (7.27%) 07-26-2026 0142-linked-list-cycle-ii Medium .cpp Hash Table

---
+++ C++/Medium/0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
@@ -0,0 +1,25 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *detectCycle(ListNode *head) {
+        unordered_map<ListNode*, int>mpp;
+        ListNode* temp=head;
+    
+        while(temp!=NULL){
+            if(mpp[temp]>0){
+                return temp;
+            }
+            mpp[temp]++;
+            temp=temp->next;
+
+        }
+        return NULL;
+    }
+};

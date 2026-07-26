d5bdf7478fb4fd5f9949dad4499a9c74a9bba4b8
Satyam Mall <44332322+satyamhub@users.noreply.github.com>
2026-07-26 11:07:39 +0530

    6 ms (72.57%) 11.4 MB (24.54%) 07-26-2026 0142-linked-list-cycle-ii Medium .cpp Hash Table

--- C++/Medium/0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
+++ C++/Medium/0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
@@ -1,3 +1,20 @@
+/*
+Approach:
+1. After slow meet fast 1st time, put slow back to head and move both slow and 
+   fast one one step at a time.
+   eventually they will meet at starting point of loop.
+   because when slow meets fast first time, it covers d distance in loop,
+   and total distance is L+d, and also L=distance between head and start of loop.
+   so fast will cover L and slow will also cover L distance from head.
+   so they will meet at starting point of head;
+
+   TC:O(N)
+   SC:O(1)
+
+
+*/
+
+
 /**
  * Definition for singly-linked list.
  * struct ListNode {
@@ -9,16 +26,20 @@
 class Solution {
 public:
     ListNode *detectCycle(ListNode *head) {
-        unordered_map<ListNode*, int>mpp;
-        ListNode* temp=head;
-    
-        while(temp!=NULL){
-            if(mpp[temp]>0){
-                return temp;
-            }
-            mpp[temp]++;
-            temp=temp->next;
+        ListNode* slow=head;
+        ListNode* fast=head;
 
+        while(fast!=NULL && fast->next!=NULL){
+            slow=slow->next;
+            fast=fast->next->next;
+            if(slow==fast){
+                slow=head;
+                while(slow!=fast){
+                    slow=slow->next;
+                    fast=fast->next;
+                }
+                return slow;
+            }
         }
         return NULL;
     }

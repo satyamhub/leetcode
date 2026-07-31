9c546b21600546c8b57a84a6210b9ce681a1c2eb
Satyam Mall <44332322+satyamhub@users.noreply.github.com>
2026-07-31 23:59:27 +0530

    0 ms (100%) 23.7 MB (83.45%) 07-31-2026 0283-move-zeroes Easy .cpp Array

--- C++/Easy/0283-move-zeroes/0283-move-zeroes.cpp
+++ C++/Easy/0283-move-zeroes/0283-move-zeroes.cpp
@@ -2,16 +2,19 @@ class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
         int n=nums.size();
-        for(int i=0; i<n; i++){
-
-            int pointer=0;
-            while(pointer<n-1){
-                if(nums[pointer]==0 && nums[pointer+1]!=0){
-                    swap(nums[pointer],nums[pointer+1]);
-                
-                }
-                pointer++;
+        int left=0;  //for zero tracking
+        int right=0;  // for non zero tracking
+        
+        while(left<n && right<n){
+            if(nums[left]==0 && nums[right]!=0){
+                swap(nums[left], nums[right]);
+            }else if(nums[left]!=0){
+                left++;
+                right++;
+            }else{
+                right++;
             }
+           
         }
     }
 };

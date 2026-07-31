6f3f1e0330bb27cb9b2b934e9ee36a890787f667
Satyam Mall <44332322+satyamhub@users.noreply.github.com>
2026-08-01 00:04:59 +0530

    3 ms (8.81%) 19.5 MB (34.52%) 08-01-2026 0167-two-sum-ii-input-array-is-sorted Medium .cpp Array

---
+++ C++/Medium/0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int n=nums.size();
+        int left=0;
+        int right=n-1;
+
+        while(left<right){
+            if(nums[left]+nums[right]==target){
+                return {left+1,right+1};
+            }else if(nums[left]+nums[right]>target){
+                right--;
+            }else{
+                left++;
+            }
+        }
+        return {-1};
+        
+    }
+};

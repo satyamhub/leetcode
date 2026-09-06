1978e0a188f872354c41220f82fa8f55162d407d
Satyam Mall <44332322+satyamhub@users.noreply.github.com>
2026-09-06 15:18:44 +0530

    0 ms (100%) 15.1 MB (72.54%) 09-06-2026 0138-copy-list-with-random-pointer Medium .cpp Hash Table

--- C++/Medium/0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
+++ C++/Medium/0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
@@ -17,21 +17,35 @@ public:
 class Solution {
 public:
     Node* copyRandomList(Node* head) {
-        Node *temp=head;
-        map<Node*, Node*>mpp;
+        Node* temp=head;
         while(temp!=NULL){
-            Node *newNode=new Node(temp->val);
-            mpp[temp]=newNode;
-            temp=temp->next;
+            Node* copyNode=new Node(temp->val);
+            copyNode->next=temp->next;
+            temp->next=copyNode;
+            temp=temp->next->next;
         }
 
+        
         temp=head;
         while(temp!=NULL){
-            Node *copyNode=mpp[temp];
-            copyNode->next=mpp[temp->next];
-            copyNode->random=mpp[temp->random];
+            Node* copyNode=temp->next;
+            if(temp->random){
+                copyNode->random=temp->random->next;
+            }else{
+                copyNode->random=NULL;
+            }
+            temp=temp->next->next;
+        }
+        temp=head;
+        Node* dummyNode=new Node(-1);
+        Node* res=dummyNode;
+        while(temp!=NULL){
+            res->next=temp->next;
+            temp->next=temp->next->next;
+            res=res->next;
             temp=temp->next;
         }
-        return mpp[head];
+        return dummyNode->next;
+
     }
 };

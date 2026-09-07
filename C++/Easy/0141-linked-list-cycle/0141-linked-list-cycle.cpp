7e2389c0ca40131f56a734532c2d3d61b20cc855
Satyam Mall <44332322+satyamhub@users.noreply.github.com>
2026-09-07 11:16:10 +0530

    8 ms (81.42%) 11.9 MB (22.2%) 09-07-2026 0141-linked-list-cycle Easy .cpp Hash Table

--- C++/Easy/0141-linked-list-cycle/0141-linked-list-cycle.cpp
+++ C++/Easy/0141-linked-list-cycle/0141-linked-list-cycle.cpp
@@ -1,19 +1,3 @@
-/*
-Approach: Tortoise and Heir approach
-
-1. I will assign two pointer one is slow and another is fast.
-2. slow will move 1 step and fast will move 2 step at a time.
-3. In a cycle slow and fast pointer will eventually meet, bcz
-   their distance between will decrease by 1 each time.
-
-TC:O(N)
-SC:O(1)
-
-
-
-*/
-
-
 /**
  * Definition for singly-linked list.
  * struct ListNode {
@@ -25,16 +9,13 @@ SC:O(1)
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
-        
         ListNode* slow=head;
         ListNode* fast=head;
         while(fast!=NULL && fast->next!=NULL){
+            
             slow=slow->next;
             fast=fast->next->next;
-            if(slow==fast){
-                return true;
-            }
-            
+            if(slow==fast) return true;
         }
         return false;
     }

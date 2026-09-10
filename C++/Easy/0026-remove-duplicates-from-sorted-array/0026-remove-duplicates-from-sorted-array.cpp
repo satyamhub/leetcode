aa09d76e0380573e11001855cd8e47e2bb0f1aa3
Satyam Mall <44332322+satyamhub@users.noreply.github.com>
2026-09-10 09:52:45 +0530

    0 ms (100%) 22.7 MB (18.53%) 09-10-2026 0026-remove-duplicates-from-sorted-array Easy .cpp Array

--- C++/Easy/0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
+++ C++/Easy/0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -3,17 +3,15 @@ public:
     int removeDuplicates(vector<int>& arr) {
             int n = arr.size();
             int left = 0;
-            int right = 0;
+            int right = 1;
             int index=0;
             while(left<=right && right<n){
-            if(arr[left]>=arr[right]){
+                if(arr[left]!=arr[right]){
+                    left++;
+                    arr[left]=arr[right];
+                }
                 right++;
-            }else{
-                swap(arr[left+1], arr[right]);
-                left++;
-            }
-            
-        }
+          }
         return left+1;
     }
 };

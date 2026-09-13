0563fac0e8558d9578d66170d553288fee5e3652
Satyam Mall <44332322+satyamhub@users.noreply.github.com>
2026-09-13 18:58:31 +0530

    0 ms (100%) 9.7 MB (99.91%) 09-13-2026 0567-permutation-in-string Medium .cpp Hash Table

--- C++/Medium/0567-permutation-in-string/0567-permutation-in-string.cpp
+++ C++/Medium/0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -1,47 +1,40 @@
 class Solution {
 public:
     bool checkInclusion(string s1, string s2) {
+
         int n = s1.size();
         int m = s2.size();
 
-        if (n > m) return false;
+        if (n > m)
+            return false;
 
-        vector<int> freq(256, 0);
+        vector<int> freq(26, 0);
 
-        // Frequency of characters in s1
         for (char ch : s1) {
-            freq[ch]++;
+            freq[ch - 'a']++;
         }
 
         int left = 0;
         int right = 0;
-        int cnt = n;
+        int cnt = 0;
 
         while (right < m) {
 
-            // Include s2[right]
-            if (freq[s2[right]] > 0) {
+            // Current character is required
+            if (freq[s2[right] - 'a'] > 0) {
+                freq[s2[right] - 'a']--;
+                cnt++;
+                right++;
+            }
+            else {
+                // Remove characters from left
+                freq[s2[left] - 'a']++;
+                left++;
                 cnt--;
             }
 
-            freq[s2[right]]--;
-            right++;
-
-            // Window size becomes n
-            if (right - left > n) {
-                freq[s2[left]]++;
-
-                if (freq[s2[left]] > 0) {
-                    cnt++;
-                }
-
-                left++;
-            }
-
-            // All characters matched
-            if (cnt == 0) {
+            if (cnt == n)
                 return true;
-            }
         }
 
         return false;
